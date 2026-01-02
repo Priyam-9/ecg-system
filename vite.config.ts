dc46156453026fdbc3c3b199197ca8b99369d9db
Priyam Patel <127947183+Priyam-9@users.noreply.github.com>
2026-01-02 21:13:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+//vite.dev changes made by me
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
