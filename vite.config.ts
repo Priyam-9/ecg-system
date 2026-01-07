67cdc4183754f28c8dae2e824468fb73647c56bf
Priyam Patel <127947183+Priyam-9@users.noreply.github.com>
2026-01-07 19:58:46 +0530

    Revise comments in vite.config.ts
    Updated comments to clarify recompilation requirements.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-///IN MY device
+///IN MY device with files beig deleted have to recompile
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-//vite.dev changes made by me
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
